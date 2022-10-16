9a59437c38e29fc79fa318b5a506d4369fd2596d
RoShainoff <58002663+RoShainoff@users.noreply.github.com>
2022-10-16 22:00:49 +0300

    Create binarySearch.cs

---
+++ Algorithms/C#/0704-Binary Search/binarySearch.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        
+        int low = 0, high = nums.Length - 1;
+
+        while (low <= high)
+        {
+            var middle = low + (high - low) / 2;
+            if (nums[middle] == target) return middle;
+            else if (nums[middle] > target) high = middle - 1;
+            else low = middle + 1;
+        }
+
+        return -1;
+    }
+}
