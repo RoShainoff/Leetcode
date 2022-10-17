6ecfbeeeb5cf24690b45c8943a63bfc1344d4ae9
RoShainoff <58002663+RoShainoff@users.noreply.github.com>
2022-10-17 22:39:02 +0300

    Create validateBinarySearchTree.cs

---
+++ Algorithms/C#/0092-Validate Binary Search Tree/validateBinarySearchTree.cs
@@ -0,0 +1,33 @@
+//-----------------------------------------------------------------------------
+// Runtime: 161ms
+// Memory Usage: 40.6 MB
+// Link: https://leetcode.com/submissions/detail/824679730/
+//-----------------------------------------------------------------------------
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsValidBST(TreeNode root)
+    {
+        return IsValidBST(root, long.MinValue, long.MaxValue);
+    }
+
+    public bool IsValidBST(TreeNode current, long low, long high)
+    {
+        if (current == null) return true;
+        if (current.val <= low || current.val >= high) return false;
+
+        return IsValidBST(current.left, low, current.val) && IsValidBST(current.right, current.val, high);
+    }
+}
